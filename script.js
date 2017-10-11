464af786520eb070320aa01315762cfb003bdf66
Stephanie Marvez <marvez@gmail.com>
2017-10-11 13:11:54 -0600

    red and black complete

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+
+for  (var i = 0; i < 81; i++) {
+  var tile = document.createElement('div');
+  var body = document.getElementsByTagName('body')[0];
+
+  document.body.prepend(tile);
+
+  tile.style.width = '11.1%';
+  tile.style.float = 'left';
+  tile.style.paddingBottom = '11.1%';
+
+  if (i%2 === 0) {
+    tile.style.backgroundColor = "red";
+  } else {
+    tile.style.backgroundColor = "black";
+  }
+}
 // Your JS goes here
